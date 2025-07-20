2f872e6591f5cda6c362876dc4b5dbbde0e6a092
sezarr <sezar8422@gmail.com>
2025-07-20 16:55:44 +0300

    Delete ınstagram-v1 directory

--- ınstagram-v1/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("startScript").addEventListener("click", async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"]
-  });
-});
